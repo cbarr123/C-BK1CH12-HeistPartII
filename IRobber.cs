1e54341ef75f9bac0ba4de0f6d41141cd012dbc5
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-10-16 14:39:54 -0500

    added IRobber Interface

--- IRobber.cs
+++ IRobber.cs
@@ -2,6 +2,9 @@ namespace heist2
 {
     public interface IRobber
     {
-        
+        string Name{get; set;}
+        int SkillLevel{get; set;}
+        int PercentageCut{get; set;}
+        void PerformSkill(Bank bank);
     }
 }
