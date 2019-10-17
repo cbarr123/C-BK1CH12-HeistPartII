6d34b98cab3451480d06efff35fa1ece7eae759e
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-10-17 15:14:43 -0500

    adding bank

--- Program.cs
+++ Program.cs
@@ -29,29 +29,53 @@ namespace heist2
             Console.WriteLine($"You have {rolodex.Count} operatives in the rolodex");
             Console.WriteLine("Please enter the name of a possible new operative:");
             string NewName = Console.ReadLine();
-            Console.WriteLine("Please select the operatives specialty:");
-            Console.WriteLine("1 - Hacker (Disables alarms)");
-            Console.WriteLine("2 - Muscle (Disarms guards)");
-            Console.WriteLine("3 - Lock Specialist (cracks vault)");
-            int NewSelection = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter the operatives skill on a scale of 1 - 100");
-            int NewSkillLevel = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter the operatives cut of the action");
-            int NewPercentageCut = int.Parse(Console.ReadLine());
-            Console.WriteLine($"You entered {NewName} with a skill of {NewSelection} and Level of {NewSkillLevel} and cut of {NewPercentageCut }.");
-            Console.WriteLine("=========================");
+            
+            while(NewName != "")
+            {
+                Console.WriteLine("Please select the operatives specialty:");
+                Console.WriteLine("1 - Hacker (Disables alarms)");
+                Console.WriteLine("2 - Muscle (Disarms guards)");
+                Console.WriteLine("3 - Lock Specialist (cracks vault)");
+                int NewSelection = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the operatives skill on a scale of 1 - 100");
+                int NewSkillLevel = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the operatives cut of the action");
+                int NewPercentageCut = int.Parse(Console.ReadLine());
+                // Console.WriteLine($"You entered {NewName} with a skill of {NewSelection} and Level of {NewSkillLevel} and cut of {NewPercentageCut }.");
+                Console.WriteLine("=========================");
+            
 
-            if(NewSelection == 1)
-            {
-                Hacker NextHacker = new Hacker(NewName, NewSkillLevel, NewPercentageCut);
-                rolodex.Add(NextHacker);
-            } else if(NewSelection == 2)
-            {
-                Muscle NextMuscle = new Muscle(NewName, NewSkillLevel, NewPercentageCut);
-            } else if(NewSelection == 3)
-            {
-                LockSpecialist NextLockSpecialist = new LockSpecialist(NewName, NewSkillLevel, NewPercentageCut);
+
+                if(NewSelection == 1)
+                {
+                    Hacker NextHacker = new Hacker(NewName, NewSkillLevel, NewPercentageCut);
+                    rolodex.Add(NextHacker);
+                    
+                } else if(NewSelection == 2)
+                {
+                    Muscle NextMuscle = new Muscle(NewName, NewSkillLevel, NewPercentageCut);
+                    rolodex.Add(NextMuscle);
+                } else if(NewSelection == 3)
+                {
+                    LockSpecialist NextLockSpecialist = new LockSpecialist(NewName, NewSkillLevel, NewPercentageCut);
+                    rolodex.Add(NextLockSpecialist);
+                }
+                Console.WriteLine("Please enter the name of a possible new operative:");
+                NewName = Console.ReadLine();
             }
+            Random RandomNumber = new Random();
+            Bank Newbank = new Bank()
+            {
+                CashOnHand = RandomNumber.Next(49_999, 1_000_000_000),
+                AlarmScore = RandomNumber.Next(0, 101),
+                VaultScore = RandomNumber.Next(0, 101),
+                SecurityGuardScore = RandomNumber.Next(0, 101)
+            };
+
+            Console.WriteLine("=========Recon Report==========");
+            Console.WriteLine();
+
+
 
             foreach(var robber in rolodex)
             {
